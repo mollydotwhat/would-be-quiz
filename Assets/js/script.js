638cd944de750fd5e21ec82d0ef81d0b10fd5962
molly <molly.salzberger@gmail.com>
2023-10-04 16:51:19 -0500

    more pseudocode. i'm doing the css last and you can't stop me.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,24 @@
 
+//questions
+//answers
+//checker
+// highscores : score, initials
+    //compare score values w/in array/object? somehow
+
+
 // var quizScoring = {
 //     right: 0,
 //     wrong: 0,
 // }
 
 //start()
-//interval timer
+//interval timer, decrements on wrong answer
+//quiz ends when all questions are answered OR the timer gets to 0.
+    //set elements to question & answers 
+    //submit button
+    //get input & compare to correct answer
+    //save changes/answers function (updating score)
+    //reset question &or set elements to new question 
+
+//local storage: score+initials (save at end)
+
