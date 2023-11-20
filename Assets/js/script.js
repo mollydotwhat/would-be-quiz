45d45a96bfd6a066e03180f3f821d81c18695151
molly <molly.salzberger@gmail.com>
2023-11-19 22:39:13 -0600

    where I stopped the last time.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,7 +4,8 @@
 //checker
 // highscores : score, initials
     //compare score values w/in array/object? somehow
-
+var rightAnswer;
+var wrongAnswer;
 
 // var quizScoring = {
 //     right: 0,
@@ -13,6 +14,28 @@
 
 //start()
 //interval timer, decrements on wrong answer
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (rightAnswer === true && timerCount > 0) {
+            rightAnswer++;
+        } else if (rightAnswer === false && timerCount > 0) {
+            wrongAnswer++
+            timerCount--;
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        console.log("time's up")
+      }
+    }, 1000);
+  }
 //quiz ends when all questions are answered OR the timer gets to 0.
     //set elements to question & answers 
     //submit button
